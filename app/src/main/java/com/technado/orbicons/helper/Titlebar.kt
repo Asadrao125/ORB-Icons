8824f034f7140c37ef5f8c15b2ad3bf5ecafcd74
asad.muqeem <raoasad125@gmail.com>
2022-01-25 18:14:07 +0500

    Initial Commit

--- app/src/main/java/com/technado/orbicons/helper/Titlebar.kt
+++ app/src/main/java/com/technado/orbicons/helper/Titlebar.kt
@@ -6,7 +6,6 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.RelativeLayout
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.technado.orbicons.activities.MainActivity
 import com.technado.orbicons.R
@@ -36,14 +35,6 @@ class Titlebar : RelativeLayout {
         binding = DataBindingUtil.inflate(inflater, R.layout.titlebar, this, true)
     }
 
-    fun hideTitleBar() {
-        resetTitlebar()
-    }
-
-    fun resetTitlebar() {
-        binding?.rlTitlebarMainLayout?.setVisibility(View.GONE)
-    }
-
     fun setTitle(getActivityContext: MainActivity, title: String) {
         setHideTitle()
         binding?.tvTitleRed?.visibility = VISIBLE
@@ -53,37 +44,12 @@ class Titlebar : RelativeLayout {
         binding?.ivMenu?.visibility = View.VISIBLE
         binding?.rlTitlebarMainLayout?.setVisibility(View.VISIBLE)
 
-        binding?.rlTitlebarMainLayout?.background =
-            ContextCompat.getDrawable(context, R.drawable.background2)
-
         binding?.tvTitleRed?.text = title
         binding?.tvTitleWhite?.text = title
-
-        binding?.ivBack?.background = null
 
         binding?.ivMenu?.setOnClickListener {
             getActivityContext.openDrawer()
         }
-
-        binding?.ivBack?.setOnClickListener(OnClickListener {
-            getActivityContext.onBackPressed()
-        })
-    }
-
-    fun setTitleTCPP(getActivityContext: MainActivity, title: String) {
-        setHideTitle()
-        binding?.tvTitleRed?.visibility = VISIBLE
-        binding?.tvTitleWhite?.visibility = GONE
-
-        binding?.ivBack?.visibility = View.VISIBLE
-        binding?.ivMenu?.visibility = View.GONE
-        binding?.rlTitlebarMainLayout?.setVisibility(View.VISIBLE)
-
-        binding?.rlTitlebarMainLayout?.background =
-            ContextCompat.getDrawable(context, R.drawable.background2)
-
-        binding?.tvTitleRed?.text = title
-        binding?.ivBack?.background = ContextCompat.getDrawable(context, R.drawable.red_shape)
 
         binding?.ivBack?.setOnClickListener(OnClickListener {
             getActivityContext.onBackPressed()
@@ -98,13 +64,9 @@ class Titlebar : RelativeLayout {
         binding?.ivBack?.visibility = View.VISIBLE
         binding?.ivMenu?.visibility = View.GONE
         binding?.rlTitlebarMainLayout?.setVisibility(View.VISIBLE)
-        binding?.rlTitlebarMainLayout?.background =
-            ContextCompat.getDrawable(context, R.drawable.background)
 
         binding?.tvTitleRed?.text = title
         binding?.tvTitleWhite?.text = title
-
-        binding?.ivBack?.background = null
 
         binding?.ivMenu?.setOnClickListener {
             getActivityContext.openDrawer()
@@ -115,14 +77,6 @@ class Titlebar : RelativeLayout {
         })
     }
 
-    fun setColor() {
-        binding?.rlTitlebarMainLayout?.setBackgroundColor(resources.getColor(R.color.trans))
-    }
-
-    fun setColor2() {
-        binding?.rlTitlebarMainLayout?.setBackgroundColor(resources.getColor(R.color.blue))
-    }
-
     fun setTitleThird(getActivityContext: MainActivity, title: String) {
         setHideTitle()
         binding?.tvTitleRed?.visibility = GONE
@@ -131,12 +85,16 @@ class Titlebar : RelativeLayout {
         binding?.ivBack?.visibility = View.GONE
         binding?.ivMenu?.visibility = View.GONE
         binding?.rlTitlebarMainLayout?.setVisibility(View.VISIBLE)
-        binding?.rlTitlebarMainLayout?.background =
-            ContextCompat.getDrawable(context, R.drawable.background)
-
-        binding?.ivBack?.background = null
 
         binding?.tvTitleWhite?.text = title
+    }
+
+    fun hideTitleBar() {
+        resetTitlebar()
+    }
+
+    fun resetTitlebar() {
+        binding?.rlTitlebarMainLayout?.setVisibility(View.GONE)
     }
 
     fun setHideTitle() {
