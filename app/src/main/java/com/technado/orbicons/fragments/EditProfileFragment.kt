a2a64478e4f0cab9ece2f7dad79e2ca13e24926b
asad.muqeem <raoasad125@gmail.com>
2022-01-25 10:44:04 +0500

    Initial Commit

---
+++ app/src/main/java/com/technado/orbicons/fragments/EditProfileFragment.kt
@@ -0,0 +1,45 @@
+package com.technado.orbicons.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.technado.demoapp.base.BaseFragment
+import com.technado.orbicons.R
+import com.technado.orbicons.databinding.DownloadsFragmentBinding
+import com.technado.orbicons.databinding.EditProfileFragmentBinding
+import com.technado.orbicons.helper.Titlebar
+
+class EditProfileFragment : BaseFragment() {
+    var binding: EditProfileFragmentBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_edit_profile, container, false)
+
+        binding?.imgUpload?.setOnClickListener(View.OnClickListener {
+
+        })
+
+        binding?.btnDone?.setOnClickListener(View.OnClickListener {
+            val fullName = binding?.edtFullName?.text.toString().trim()
+            if (fullName.isEmpty()) {
+                binding?.edtFullName?.setError("Full Name Required")
+                binding?.edtFullName?.requestFocus()
+            } else {
+                getActivityContext!!.onBackPressed()
+            }
+        })
+
+        return binding!!.root
+    }
+
+    override fun setTitlebar(titlebar: Titlebar) {
+        titlebar.setTitleSecond(getActivityContext!!, "Edit Profile")
+    }
+}
---
+++ app/src/main/java/com/technado/orbicons/fragments/TCandPPFragment.kt
@@ -0,0 +1,29 @@
+package com.technado.orbicons.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.technado.demoapp.base.BaseFragment
+import com.technado.orbicons.R
+import com.technado.orbicons.databinding.ChangePasswordFragmentBinding
+import com.technado.orbicons.databinding.TCandPPPasswordFragmentBinding
+import com.technado.orbicons.helper.Titlebar
+
+class TCandPPFragment(val title: String) : BaseFragment() {
+    var binding: TCandPPPasswordFragmentBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_t_cand_p_p, container, false)
+        return binding!!.root
+    }
+
+    override fun setTitlebar(titlebar: Titlebar) {
+        titlebar.setTitleSecond(getActivityContext!!, title)
+    }
+}
